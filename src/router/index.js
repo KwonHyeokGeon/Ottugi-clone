7ad0745b33a345c4d9c9b74f9120b0f58f6c2327
KwonHyeokGeon <107496661+KwonHyeokGeon@users.noreply.github.com>
2023-05-11 11:31:38 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/ottugi/",
+    path: "/",
     name: "HomePage",
     component: () => import("@/pages/HomePage"),
   },
-  { path: "/ottugi/Detail", 
+  { path: "/Detail", 
     name: "DetailPage", 
     component: () => import("@/pages/DetailPage") },
 ];
